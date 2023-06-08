9b5497ee9a9e88ba653f0a9f145d629b8d409e7a
jnaoroji <jennynaoroji@gmail.com>
2023-06-08 13:40:53 +1000

    displaying tables functional

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,13 @@
-const Sequelize = require('sequelize');
-
 // Enable access to .env variables
 require('dotenv').config();
 
 // Use environment variables to connect to database
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  },
-  console.log(`Connected to the mybusiness_db database.`)
-);
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host:'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-module.exports = sequelize;
+module.exports = connection;
